653e17155cedca8b32094f84b5bf249e048874d4
sss <sujianfu199@qq.com>
2019-06-19 10:52:34 +0800

    Initial Commit

---
+++ SIOSTool/SIOSTool.h
@@ -0,0 +1,19 @@
+//
+//  SIOSTool.h
+//  SIOSTool
+//
+//  Created by SSS on 2019/6/19.
+//  Copyright © 2019 SSS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SIOSTool.
+FOUNDATION_EXPORT double SIOSToolVersionNumber;
+
+//! Project version string for SIOSTool.
+FOUNDATION_EXPORT const unsigned char SIOSToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SIOSTool/PublicHeader.h>
+
+
